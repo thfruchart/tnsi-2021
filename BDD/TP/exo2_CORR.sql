bce50df0852b62654aceaddcdcfd700da97e0f06
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-11-22 15:02:29 +0100

    Update exo2_CORR.sql

--- BDD/TP/exo2_CORR.sql
+++ BDD/TP/exo2_CORR.sql
@@ -28,10 +28,28 @@ JOIN emprunt ON livre.isbn = emprunt.isbn
 WHERE emprunt.retour<'2020-03-31';
 
 -- 5.Afficher le nom et le prénom des usagers ayant emprunté un livre
+SELECT  usager.prenom, usager.nom 
+FROM usager
+JOIN emprunt ON emprunt.code_barre   = usager.code_barre   ; 
 
 
 -- 6.Même requête que la précédente, en supprimant les doublons
+SELECT  DISTINCT  usager.prenom, usager.nom 
+FROM usager
+JOIN emprunt ON emprunt.code_barre   = usager.code_barre   ; 
+
 
 -- 7.Même requête que la précédent, en affichant les noms triés par ordre alphabétique
+SELECT  DISTINCT  usager.prenom, usager.nom 
+FROM usager
+JOIN emprunt ON emprunt.code_barre   = usager.code_barre  
+ORDER BY  usager.nom  ASC ; 
 
--- 8.Afficher le nom et le prénom de tous les usagers habitant le 5ème arrondissement (code postal = 75005) ayant emprunté un livre : on fera également afficher le titre du livre et la date de retour prévue.
+-- 8.Afficher le nom et le prénom de tous les usagers habitant le 5ème arrondissement (code postal = 75005)
+--  ayant emprunté un livre : on fera également afficher le titre du livre et la date de retour prévue.
+SELECT  DISTINCT  usager.prenom, usager.nom , emprunt.retour, livre.titre
+FROM usager
+JOIN emprunt ON emprunt.code_barre   = usager.code_barre  
+JOIN livre ON livre.isbn = emprunt.isbn 
+WHERE usager.cp='75005'
+ORDER BY  usager.nom  ASC ; 
