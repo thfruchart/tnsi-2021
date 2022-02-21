0e11a8a8c4c40f16561a1603219b3327a0019a41
thfruchart <66477650+thfruchart@users.noreply.github.com>
2022-02-21 16:39:21 +0100

    Create exmple1.php

---
+++ php/exmple1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<table border=1>
+<tr> <td> x </td>
+
+<?php
+for($j = 1; $j <= 10; $j++)
+   { echo "<td><b> $j </b> </td>";}
+?>
+</tr>
+ 
+</table>
+</body>
+</html>
